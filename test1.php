675ce6a7711986f36a547ec3ed662dcf2c70a22f
sunarmzaa <sunarmzaa@gmail.com>
2021-07-25 12:11:35 +0700

    csv

---
+++ test1.php
@@ -0,0 +1,43 @@
+<?php
+$login = 'arm';
+$pass = '123';
+
+if(($_SERVER['PHP_AUTH_PW']!= $pass || $_SERVER['PHP_AUTH_USER'] != $login)|| !$_SERVER['PHP_AUTH_USER'])
+{
+    $user = $_SERVER['PHP_AUTH_USER'];
+    setcookie("PHP_AUTH_USER", $user, time()+60,'/'); // 5 Minutes !
+    
+    header('WWW-Authenticate: Basic realm="Test auth"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Auth failed';
+    exit;
+}
+// $user = $_SERVER['PHP_AUTH_USER'];
+// setcookie("PHP_AUTH_USER", $user, time()+60,'/'); // 5 Minutes !
+?>
+
+<?php
+$conn = mysqli_connect("localhost", "root", "", "phppot_examples");
+
+$filename = "toy_csv.csv";
+$fp = fopen('php://output', 'w');
+
+$query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA='phppot_examples' AND TABLE_NAME='toy'";
+
+$result = mysqli_query($conn,$query);
+while ($row = mysqli_fetch_row($result)) {
+	$header[] = $row[0];
+}	
+
+header('Content-type: application/csv');
+header('Content-Disposition: attachment; filename='.$filename);
+fputcsv($fp, $header);
+
+$query = "SELECT * FROM toy";
+$result = mysqli_query($conn, $query);
+while($row = mysqli_fetch_row($result)) {
+	fputcsv($fp, $row);
+}
+exit;
+?>
